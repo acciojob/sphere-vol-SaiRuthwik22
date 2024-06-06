f8ee7d2ce6db7476625c69bc5f5cb21a8908ce66
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-06 22:17:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    let radius = document.getElementById("radius").value;
+    let radiusNum = Number(radius);
+    
+    if (!isNaN(radiusNum) && radiusNum >= 0) {
+        let volume = (4/3) * Math.PI * Math.pow(radiusNum, 3);
+        document.getElementById("volume").value = volume.toFixed(4);
+    } else {
+        document.getElementById("volume").value = NaN;
+    }
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
